38f141a88e4a2cf355312749d587ea582cdc4d23
Siskodemon <whiteprist@gmail.com>
2024-06-19 23:45:17 +0300

    Done

---
+++ src/main/java/com/evilcorp/Spring_Security/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.evilcorp.Spring_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
